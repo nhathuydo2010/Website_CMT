9f785de6156578c8afbb8c2eb7e021740cea98a6
donhathuy521h0237 <521h0237@student.tdtu.edu.vn>
2025-08-08 18:18:14 +0700

    finall 8/8

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -3,7 +3,7 @@ export default function TopBar() {
     <div className="w-full bg-white text-red-700 text-xs sm:text-sm md:text-[14px] font-medium text-center px-2 sm:px-4 py-1 sm:py-2 overflow-hidden">
       <div className="animate-marquee whitespace-nowrap">
         Quý khách hàng có nhu cầu tư vấn Mua & Thuê máy Photocopy vui lòng liên hệ phòng kinh doanh 
-        <span className="font-semibold"> 0908.100.201 </span> 
+        <span className="font-semibold"> 0944.333.986 </span> 
         - Hoặc Hotline: <span className="font-semibold">(84) 38226165 - 38226166 - 38296594</span>
       </div>
     </div>
